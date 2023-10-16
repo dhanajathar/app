b2d25e99ef4ab1ea3ef720d8e529f8b458fff3d8
dhanajathar <dhana.jathar@gmail.com>
2023-10-16 16:33:09 +0530

    D Alert changes

--- src/components/DAlertBox/index.jsx
+++ src/components/DAlertBox/index.jsx
@@ -1,35 +1,47 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import './DAlertBox.css';
 import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined';
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
+import CloseIcon from '@mui/icons-material/Close';
 
 const DAlertBox = ({ errorText, warningText }) => {
-  const [expanded, setExpanded] = useState(false);
-  const toggleExpansion = () => {
-    setExpanded(!expanded);
-  };
+    const [expanded, setExpanded] = useState(false);
+    const [clearWarning, setClearWarning] = useState(false);
+    const toggleExpansion = () => {
+        setExpanded(!expanded);
+    };
 
-  const divStyle = useMemo(
-    () => ({
-      overflow: expanded ? 'visible' : 'hidden',
-      textOverflow: expanded ? 'clip' : 'ellipsis',
-      whiteSpace: expanded ? 'normal' : 'nowrap'
-    }),
-    [expanded]
-  );
+    const divStyle = useMemo(() => ({
+        overflow: expanded ? 'visible' : 'hidden',
+        textOverflow: expanded ? 'clip' : 'ellipsis',
+        whiteSpace: expanded ? 'normal' : 'nowrap'
+    }), [expanded]);
 
-  return (
-    <>
-      {' '}
-      {(errorText || warningText) && (
-        <div className={warningText ? 'warning-box arrow-box' : 'error-box arrow-box'}>
-          <ErrorOutlineOutlinedIcon className='error-icon' />
-          <div style={divStyle} onClick={toggleExpansion}>
-            <span>{errorText ? errorText : warningText}</span>
-          </div>
+    useEffect(() => {
+        setClearWarning(false)
+    }, [warningText])
+
+    return (<> {errorText ? <div className={'error-box alert-box'} >
+        <ErrorOutlineOutlinedIcon className='error-icon' />
+        <div style={divStyle} onClick={toggleExpansion}>
+            <span>
+                {errorText}
+            </span>
         </div>
-      )}
+    </div> : <>
+        {warningText && !clearWarning && <div className={'warning-box alert-box'} >
+            <WarningAmberIcon className='warning-icon' />
+            <div>
+                <span>
+                    {warningText}
+                </span>
+            </div>
+            <CloseIcon className='alert-close-icon' onClick={() => setClearWarning(true)} />
+        </div>}
     </>
-  );
+    }
+    </>
+    );
 };
 
 export default DAlertBox;
